dad7d5ec018540edcba1a27d9cdd3bbf7198d7b3
sadhana-12345 <psadhana582003@gmail.com>
2024-09-13 15:30:30 +0530

    deploy

--- src/main/java/com/ojt/project/Fit4LifeApplication.java
+++ src/main/java/com/ojt/project/Fit4LifeApplication.java
@@ -2,11 +2,10 @@ package com.ojt.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Fit4LifeApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(Fit4LifeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Fit4LifeApplication.class, args);
+	}
 }
