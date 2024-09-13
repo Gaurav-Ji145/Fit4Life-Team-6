9dbbd09ceff2dca08cec0c92d43afe3715dcd675
sadhana-12345 <psadhana582003@gmail.com>
2024-09-13 13:46:20 +0530

    deploy

--- src/main/java/com/ojt/project/Fit4LifeApplication.java
+++ src/main/java/com/ojt/project/Fit4LifeApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Fit4LifeApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(Fit4LifeApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Fit4LifeApplication.class, args);
+        String activeProfile = context.getEnvironment().getActiveProfiles().length == 0 ? "default" : String.join(",", context.getEnvironment().getActiveProfiles());
+        System.out.println("Active Profile: " + activeProfile);
+    }
 }
+
