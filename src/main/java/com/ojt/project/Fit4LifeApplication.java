5b04e073fd17a1030339a8a553e89af0e4d5fa1b
sadhana-12345 <psadhana582003@gmail.com>
2024-09-13 14:34:44 +0530

    update e

--- src/main/java/com/ojt/project/Fit4LifeApplication.java
+++ src/main/java/com/ojt/project/Fit4LifeApplication.java
@@ -2,14 +2,12 @@ package com.ojt.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Fit4LifeApplication {
-
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Fit4LifeApplication.class, args);
-        String activeProfile = context.getEnvironment().getActiveProfiles().length == 0 ? "default" : String.join(",", context.getEnvironment().getActiveProfiles());
-        System.out.println("Active Profile: " + activeProfile);
+        // Your code here
     }
 }
-
